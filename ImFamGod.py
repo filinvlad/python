9b77f3094c73ea68d469ca20d35aedf2b88fc03e
filinvlad <114906816+filinvlad@users.noreply.github.com>
2023-01-16 16:19:17 +0300

    Delete ImFamGod.py

--- ImFamGod.py
+++
@@ -1,4 +0,0 @@
-def my_dan(name, family, year, sity, email, tel):
-    return "".join([name, family, year, sity, email, tel])
-
-print(my_dan(name = "Вася", family = "Иванов", year = "1999", sity = "Химки", email = "VS@mail.ru", tel = "8777777777"))
